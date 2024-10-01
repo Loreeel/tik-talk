da3dafca55232440365e2d783e7a1920cfbbbb61
Loreel <andriy.juravel2000@gmail.com>
2024-10-01 18:07:55 +0300

    Start dev login and profile page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProfileCardComponent } from "./common-ui/profile-card/profile-card.component";
-
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -10,5 +9,5 @@ import { ProfileCardComponent } from "./common-ui/profile-card/profile-card.comp
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'tik-talk';
+
 }
